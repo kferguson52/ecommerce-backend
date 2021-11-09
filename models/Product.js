caf38e47c64e420081a6816106e1e8e707876c41
Kyle Ferguson <kyle@Kyles-MBP.hsd1.tx.comcast.net>
2021-11-08 20:43:03 -0600

    finish the product ORM file. Need to finish last 2 object files in the ORM / sequelize format

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,48 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes, INTEGER } = require('sequelize');
 // import our database connection from config.js
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // define an id column
+    id: {
+      // use the special Sequelize DataTypes object provide what type of data it is
+      type: DataTypes.INTEGER,
+      // this is the equivalent of SQL's `NOT NULL` option
+      allowNull: false,
+      // instruct that this is the Primary Key
+      primaryKey: true,
+      // turn on auto increment
+      autoIncrement: true
+    },
+    // define a username column
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        customValidator(value) {
+          if (value != INTEGER) {
+            throw new Error("This has to be a number");
+          }
+        }
+      },
+      category_id: {
+        type: DataTypes.INTEGER
+      }
+    }
   },
   {
     sequelize,
